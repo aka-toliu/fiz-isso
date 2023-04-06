1c9b46d44ff5a2b9247288476a3e4858a07c6f5f
Túlio <tulio@unip.br>
2023-04-06 14:58:47 -0300

    estilização tela de login

---
+++ v1/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDtiL4KTx_yImv77BLSz2CEVRvny4BeEeE",
+    authDomain: "fiz-isso.firebaseapp.com",
+    projectId: "fiz-isso",
+    storageBucket: "fiz-isso.appspot.com",
+    messagingSenderId: "525291205680",
+    appId: "1:525291205680:web:7f95433896c3676a6aa9f5",
+    measurementId: "G-9ZL657CFZP"
+  }
+};
--- v2/src/app/app-routing.module.ts
+++ v2/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { LoginComponent } from './view/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'login', component: LoginComponent }, 
+  
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
