83b388e3adba6cad7afce66229e6c78cc2539e3f
Túlio <tulio@unip.br>
2023-04-12 19:29:19 -0300

    estilização card de registro

--- v2/src/app/shared/card-registro/card-registro.component.ts
+++ v2/src/app/shared/card-registro/card-registro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-registro',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-registro.component.scss']
 })
 export class CardRegistroComponent implements OnInit {
+
+  @Input() public titulo!: string;
+  @Input() public icone!: string;
+  @Input() public cor!: string;
+  @Input() public status!: string;
+  @Input() public periodo!: string;
+  @Input() public horario!: string;
+  @Input() public historico!: any;
+
+  selected: boolean = false;
 
   constructor() { }
 
--- v2/src/app/view/home/home.component.ts
+++ v2/src/app/view/home/home.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  registros = [
+    {
+      titulo: "Teste",
+      icone: "default",
+      cor: "blue",
+      status: null,
+      periodo: "diario",
+      horario: "09:00",
+      historico: [
+        {
+          titulo: "Teste",
+          icone: "default",
+          cor: "blue",
+          data: Date.now()
+        }
+      ]
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
